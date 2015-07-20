6c6b876fa4b70ba418c97919244ff20f48ca0a17
Anthony Mariano <anthony@impossible-bureau.com>
2015-07-20 12:31:37 -0600

    Revert changes.

--- source/js/app.js
+++ source/js/app.js
@@ -4,5 +4,5 @@ window._ = require('underscore');
 window.ib = {};
 
 require('controllers/example_controller');
-require('Events');
+// require('Events');
 // require('controllers/VideoController');
